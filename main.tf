571ced12c25a829257630c19f093c007f6eaf706
Lukasz Pawlik <lukepawlik@gmail.com>
2021-10-12 09:52:53 +0200

    exercise 1 solution

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
-data "aws_region" "current" {}
+locals {
+  app_folder = "app"
+}
+
+resource "aws_s3_bucket" "frontend_application" {
+  bucket = "lpawlik-selleo-frontend-application"
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "frontend_application" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.frontend_application.arn}/${local.app_folder}/*"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_application" {
+  bucket = aws_s3_bucket.frontend_application.id
+  policy = data.aws_iam_policy_document.frontend_application.json
+}
